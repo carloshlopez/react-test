66663d9418da5e42068df57f9f6ffe0c6e4b714a
Carlos López <carloshlopez@gmail.com>
2018-02-08 20:04:50 +0000

    componentes con estado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(){
+    super();
+    this.changeTitle = this.changeTitle.bind(this);
+    
+    this.state = {
+      title: "El titulo papá"
+    }
+  }
+  
+  changeTitle(){
+    this.setState({
+      title: "A very cool title"
+    });
+  }
+  
   render() {
     return (
-        <h1>Hey</h1>
+        <div>
+          <h1>Hey {this.state.title} </h1>
+          <button onClick={this.changeTitle}>Cambiar Tit</button>
+        </div>
     );
   }
+  
+  
 }
 
 export default App;
