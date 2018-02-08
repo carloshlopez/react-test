97d44652ee9ace401b92e71872fe1ae88c71575a
Carlos López <carloshlopez@gmail.com>
2018-02-08 19:57:40 +0000

    Componentes con propiedades

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import Welcome from './Welcome.js'
 import './App.css';
 
 class App extends Component {
   render() {
+    const names = ['a name', 'another nbane', 'Laura'];
     return (
-        <h1>Hey</h1>
+        <div>
+        <Welcome name="Pepe" />
+        <Welcome name="Pepe 2" />
+                
+        {names.map(name =>
+          <Welcome name={name}/>
+        )
+          
+        }
+                
+        </div>
     );
   }
 }
