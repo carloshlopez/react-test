c0557e7aa14bc919220c008c0f32c4c71ee2812e
Carlos López <carloshlopez@gmail.com>
2018-02-08 22:52:41 +0000

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Home = () => <h2>Hos sme</h2>;
+
+const About = () => <h2>This is the About</h2>;
+
+const NoMatch = () => <h1>Paila</h1>;
+
+const Message = (props) => <p>Este es el mensaje {props.params.id}</p>
+
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
 
 class App extends Component {
   render() {
     return (
-        <h1>Hey</h1>
+          <Router>
+            <div>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/topics">Topics</Link></li>
+              </ul>
+        
+              <hr/>
+        
+              <Route exact path="/" component={Home}/>
+              <Route path="/about" component={About}/>
+              <Route path="/topics" component={Topics}/>
+            </div>
+          </Router>
     );
   }
 }
---
+++ src/Welcome.js
@@ -0,0 +1,4 @@
+import React from 'react';
+const Welcome = (props) => <h1>Hey {props.name}!!!</h1>;
+
+export default Welcome;
