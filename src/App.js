170030324798d223fa9b89cf285e889dc0fa54bc
Carlos López <carloshlopez@gmail.com>
2018-02-08 21:05:55 +0000

    bootstrap button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
+      <div>
         <h1>Hey</h1>
+         
+      <Button bsStyle="primary">Primary</Button>
+      </div>
     );
   }
 }
