66e9aef28c12f754a26bb8691cec2ee02398b5d3
Carlos López <carloshlopez@gmail.com>
2018-02-08 20:58:00 +0000

    http con axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    
+    this.state = {
+      names: []
+    }
+    
+    axios.get("http://react-test-carloshlopez.c9users.io:8081/names")
+    .then(resp => {
+      console.log("OK api call", resp);
+      this.setState({
+        names: resp.data
+      })
+    })
+    .catch(error =>{
+      console.log("ERROR api call", error)
+    })
+    
+  }
+  
   render() {
     return (
+        <div>
         <h1>Hey</h1>
+        <p>This are the names</p>
+        <ul>
+          {this.state.names.map(name =>
+            <li key={name}>{name}</li>
+          )}
+        </ul>
+        </div>
+        
     );
   }
 }
