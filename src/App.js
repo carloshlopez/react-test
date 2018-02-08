021c8430a58af36b6140289462a2580a40f866c5
Carlos López <carloshlopez@gmail.com>
2018-02-08 20:32:24 +0000

    forms

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: "default"
+    }
+  }
+  
   render() {
     return (
-        <h1>Hey</h1>
+        <div>
+          <h1>Hey</h1>
+          <input type="text" value={this.state.name} onChange={this.updateName.bind(this)}/>
+          <button onClick={this.sayHi.bind(this)}>Hi!</button>
+        </div>
     );
   }
+  
+  sayHi(){
+    console.log("Hi!!", this.state.name);
+  }
+  
+  updateName(e){
+    this.setState({
+      name: e.target.value
+    })
+  }
+  
 }
 
 export default App;
